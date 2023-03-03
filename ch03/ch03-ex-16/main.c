a0f573b3c5d2c23ed8e83a2a4721c746845203b3
KELLSPING\kells <kellsping@gmail.com>
2023-03-03 15:05:28 +0800

    Update. Add content to chapter 3.

---
+++ ch03/ch03-ex-16/main.c
@@ -0,0 +1,10 @@
+/* ch03-ex-16 */
+#include <stdio.h>
+
+int main(){
+    printf("sizeof(unsigned int): %lld\n", sizeof(unsigned int));
+    printf("sizeof(double): %lld\n", sizeof(double));
+    printf("sizeof(unsigned short int): %lld\n", sizeof(unsigned short int));
+
+    return 0;
+}

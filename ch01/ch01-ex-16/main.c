e18d19047c1816d55e2c88af3396e630b802e8e6
KELLSPING\kells <kellsping@gmail.com>
2023-03-03 10:49:26 +0800

    Update. Complete the coding example in chapter 1.

---
+++ ch01/ch01-ex-16/main.c
@@ -0,0 +1,12 @@
+/* ch01-ex-16 */
+#include <stdio.h>
+
+int main(){
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");
+    printf(" *******\n");
+    printf("*********\n");
+    
+    return 0;
+}
---
+++ ch01/ch01-ex-17/main.c
@@ -0,0 +1,13 @@
+/* ch01-ex-17 */
+#include <stdio.h>
+
+int main(){
+    printf("********\n");
+    printf("********\n");
+    printf("**    **\n");
+    printf("**    **\n");
+    printf("********\n");
+    printf("********\n");
+    
+    return 0;
+}
